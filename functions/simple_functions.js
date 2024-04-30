c428dec8568132930d9158a7abf868e985a9ea67
Dede Augustine <bookdedeaugustine@gmail.com>
2024-04-30 13:04:52 -0500

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,30 @@
+"use strict"
+
+
+//function to display my name, favorite movie, and favorite band
+function favroiteThings() {
+
+    console.log("My name is Dede");
+    console.log("My favorite movie is Prisoners");
+    console.log("My favorite band is Jackson 5");
+
+}
+
+//fuction to display whyImhere
+function whyImHere() {
+
+    console.log("I am here because I want to expand my knowledge and try something new");
+
+}
+
+function favoritePlace() {
+
+    console.log("My favorite place is The Studio");
+
+}
+
+
+//calling/invoking our functions
+favroiteThings();
+whyImHere();
+favoritePlace();
